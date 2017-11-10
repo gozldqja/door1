e009fce36ffbb5549b4d418dc639b340bb70bf10
gozldqja <33479585+gozldqja@users.noreply.github.com>
2017-11-10 14:58:22 +0900

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>전달받음</h1>
+    <?php
+    echo $_REQUEST['pinD2']."값 받음";
+
+    ?>
+
+  </body>
+  </html>
